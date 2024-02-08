22118aadc06b3dfb7d57b5d04a8ba3f1cdfbd1d0
Ali Jahani <81471730+alipyth@users.noreply.github.com>
2024-02-08 11:58:40 +0330

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from requests.structures import CaseInsensitiveDict
+
+url = "https://admin.alanchand.com/api/arz"
+
+headers = CaseInsensitiveDict()
+headers["Content-Type"] = "application/json"
+
+data = '{"lang":"fa"}'
+
+
+resp = requests.post(url, headers=headers, data=data)
+
+#print(resp.json()) #for see data structure
+
+print(f"{resp.json()['arz'][0]['price'][0]['price']} ----------- {resp.json()['arz'][0]['price'][0]['updated_at']}") #this is dollar price
